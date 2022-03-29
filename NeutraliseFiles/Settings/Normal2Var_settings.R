7aa0364ed6e4742d9abdb73a26aebbfbcf20fb88
lucp9827 <54359880+lucp9827@users.noreply.github.com>
2022-03-29 18:21:00 +0200

    Delete directories

--- NeutraliseFiles/Settings/Normal2Var_settings.R
+++
@@ -1,8 +0,0 @@
-data.generator.name<-"Normal2var"
-
-settings<-data.frame(
-  delta=c(0,1,0,2,3),
-  sd1=c(1,1,2,2,3),
-  sd2=c(2,2,3,3,4),
-  null=c(1,0,1,0,0)
-)
--- NeutraliseFiles/Settings/Normal_settings.R
+++
@@ -1,5 +0,0 @@
-settings<-data.frame(
-  delta=c(0,0.5,1.5,2.5),
-  sd=c(1,3,3,3),
-  null=c(1,0,0,0)
-)
