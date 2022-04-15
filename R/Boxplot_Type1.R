d3fe84ba560e5d3aea10563f055fdcebe3183546
lucp9827 <leyla.kodalci@uhasselt.be>
2022-04-15 09:22:44 +0200

    updates report

--- R/Boxplot_Type1.R
+++ R/Boxplot_Type1.R
@@ -1,9 +1,10 @@
 #' @export
 Boxplot_TypeI<-function(path,method,alpha=0.05,tol=0.02,panel="",ylim=c(0,0.07)) {
+
   finished<-read.csv(paste(path,"/Results/Finished.txt",sep=""),
                      header=T)
   data<-finished$data[finished$method==method]
-  
+
   cnt.scenarios<-0
   cnt<-1
   pwr<-c()
@@ -15,34 +16,39 @@ Boxplot_TypeI<-function(path,method,alpha=0.05,tol=0.02,panel="",ylim=c(0,0.07))
     file1<-paste(path,"/Results/SimRes_",method,"_",d,
                  "/",dir1[grepl(".RData",dir1)&grepl(method,dir1)],
                  sep="")
-    
+
     load(file1)
     results1[[cnt]]<-results
-    pwr<-c(pwr,results$power.05[results$null==1])
+
+    x= colnames(results)
+    colnr = grep(alpha,x)
+
+
+    pwr<-c(pwr,results[results$null==1,colnr])
     distr<-c(distr,results$distribution[results$null==1])
     n<-c(n,results$n1[results$null==1]+results$n2[results$null==1])
-    
+
     cnt<-cnt+1
     cnt.scenarios<-cnt.scenarios+length(pwr)
   }
-  
+
   #boxplot(pwr,
   #        ylab=paste("Type I error rate of ",
   #                   deparse(substitute(method)),sep=""))
-  
-  
+
+
   cat(paste(method," has on average a type I error rate of ",
             round(100*pwr/cnt.scenarios,2),"% at the nominal ",
             alpha," level.\n",sep=""))
   cat(paste(method," has a type I error rate not larger than ",
             alpha," + ",tol," in ",
-            round(100*mean(pwr<alpha+tol),1), 
+            round(100*mean(pwr<alpha+tol),1),
             "% of the scenarios.\n",sep=""))
-  
+
   db<-data.frame(pwr=pwr,distribution=distr,n=n)
   invisible(list(power=db,
                  results=results1))
-  
+
   if(panel=="") {
     p0<-ggplot(db,aes(x="",y=pwr))
   }
@@ -60,5 +66,4 @@ Boxplot_TypeI<-function(path,method,alpha=0.05,tol=0.02,panel="",ylim=c(0,0.07))
                {{method}},sep=""))+
     geom_hline(yintercept=alpha, linetype="dotted", colour="red")+
     xlab("")
-  
 }
--- R/Power_QQ.R
+++ R/Power_QQ.R
@@ -2,37 +2,47 @@
 Power_QQ<-function(path,method1,method2,alpha=0.05,
                    par.fix=NULL,
                    data=NULL,
-                   col="black", add.to.plot=NULL) {
+                   col="black", add.to.plot=NULL) { # data = data generation tool
+
+  # Read finished file
   finished<-read.csv(paste(path,"/Results/Finished.txt",sep=""),
                      header=T)
+
+  # Save data generation methods ran per specified method
   data1<-finished$data[finished$method==method1]
   data2<-finished$data[finished$method==method2]
   data.i<-intersect(data1,data2)
-  
+
   results1<-list()
   results2<-list()
-  
+
   win2<-0
   cnt.scenarios<-0
   cnt<-1
   pwr1<-c()
   pwr2<-c()
+
   for(d in data.i) {
+
+    # specify folder in results file
     dir1<-dir(paste(path,"/Results/SimRes_",method1,"_",d,sep=""))
     dir2<-dir(paste(path,"/Results/SimRes_",method2,"_",d,sep=""))
+
+
     file1<-paste(path,"/Results/SimRes_",method1,"_",d,
                  "/",dir1[grepl(".RData",dir1)&grepl(method1,dir1)],
                  sep="")
+
     file2<-paste(path,"/Results/SimRes_",method2,"_",d,
                  "/",dir2[grepl(".RData",dir2)&grepl(method2,dir2)],
                  sep="")
-    
+
     load(file1)
     if(!is.null(data)) {
       results<-results[results$distribution==data,]
     }
     settings.fix<-results%>%select(names(par.fix))
-    
+
     if(!is.null(par.fix)) {
       results<-results[apply(settings.fix,1,
                              function(x) {
@@ -40,8 +50,11 @@ Power_QQ<-function(path,method1,method2,alpha=0.05,
                              }),]
     }
     results1[[cnt]]<-results
-    pwr1.tmp<-results$power.05[results$null==0]
-    
+
+    x= colnames(results)
+    colnr = grep(alpha,x)
+    pwr1.tmp<-results[(results$null==0),colnr]
+
     load(file2)
     if(!is.null(data)) {
       results<-results[results$distribution==data,]
@@ -54,28 +67,32 @@ Power_QQ<-function(path,method1,method2,alpha=0.05,
                              }),]
     }
     results2[[cnt]]<-results
-    pwr2.tmp<-results$power.05[results$null==0]
-    
+
+    x= colnames(results)
+    colnr = grep(alpha,x)
+
+    pwr2.tmp<-results[(results$null==0),colnr]
+
     cnt<-cnt+1
-    
+
     win2<-win2+sum(pwr1.tmp<pwr2.tmp)
     cnt.scenarios<-cnt.scenarios+length(pwr1.tmp)
-    
+
     pwr1<-c(pwr1,pwr1.tmp)
     pwr2<-c(pwr2,pwr2.tmp)
   }
   powers<-data.frame(pwr1=pwr1,pwr2=pwr2)
-  
+
   cat(paste(method2," wins over ",method1, " in ", round(100*win2/cnt.scenarios,1),
             "% of the ", cnt.scenarios, " scenarios",sep=""))
-  
+
   if(is.null(add.to.plot)) {
     p<-ggplot(powers,aes(x=pwr1,y=pwr2))+
       geom_point(colour={{col}})+
       ylim(0,1)+xlim(0,1)+
       geom_abline()+
-      xlab(paste("power of ",deparse(substitute(method1)),sep=""))+
-      ylab(paste("power of ",deparse(substitute(method2)),sep=""))
+      xlab(paste("power of ",method1,sep=""))+
+      ylab(paste("power of ",method2,sep=""))
   }
   else {
     p<-add.to.plot+
@@ -83,11 +100,11 @@ Power_QQ<-function(path,method1,method2,alpha=0.05,
                  colour={{col}})
   }
   p
-  
+
   invisible(list(win.pct=win2/cnt.scenarios,
                  power=powers,
                  results1=results1,
                  results2=results2,
                  graph=p))
-  
+
 }
