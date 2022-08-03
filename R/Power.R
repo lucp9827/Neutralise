4aeff66b156042a97ca51a5554882201a52f9fc9
lucp9827 <leyla.kodalci@uhasselt.be>
2022-08-03 10:20:56 +0200

    Update Power.R

--- R/Power.R
+++ R/Power.R
@@ -13,9 +13,9 @@ Power<-function(n1,n2,parameters,N=N) {
   pval_0.05 = mean(p.values<0.05)
   pval_0.1 = mean(p.values<0.1)
 
-  q_0.01 = qnorm(1-(0.01/2))
-  q_0.05 = qnorm(1-(0.05/2))
-  q_0.1 = qnorm(1-(0.1/2))
+  q_0.01 = round(qnorm(1-(0.01/2)),digit=3)
+  q_0.05 = round(qnorm(1-(0.05/2)),digit=3)
+  q_0.1 = round(qnorm(1-(0.1/2)),digit=3)
 
   ci_0.01=c(pval_0.01-q_0.01*sqrt(pval_0.01*(1-pval_0.01)/N),pval_0.01+q_0.01*sqrt(pval_0.01*(1-pval_0.01)/N))
   ci_0.05=c(pval_0.05-q_0.05*sqrt(pval_0.05*(1-pval_0.05)/N),pval_0.05+q_0.05*sqrt(pval_0.05*(1-pval_0.05)/N))
