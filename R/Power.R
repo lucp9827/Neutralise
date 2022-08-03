d0b14563869730040befaf44aadf7d95cea81c37
lucp9827 <leyla.kodalci@uhasselt.be>
2022-08-03 09:23:11 +0200

    CI power

--- R/Power.R
+++ R/Power.R
@@ -1,6 +1,6 @@
 #'
 #' @export
-Power<-function(n1,n2,parameters,N=100) {
+Power<-function(n1,n2,parameters,N=N) {
   p.values<-numeric(N)
   for(i in 1:N) {
     db<-Data.Generator(n1=n1,
@@ -9,8 +9,20 @@ Power<-function(n1,n2,parameters,N=100) {
     res<-Test(db)
     p.values[i]<-res$p.value
   }
-  return(c(mean(p.values<0.01),
-           mean(p.values<0.05),
-           mean(p.values<0.10)))
+  pval_0.01 = mean(p.values<0.01)
+  pval_0.05 = mean(p.values<0.05)
+  pval_0.1 = mean(p.values<0.1)
+
+  q_0.01 = qnorm(1-(0.01/2))
+  q_0.05 = qnorm(1-(0.05/2))
+  q_0.1 = qnorm(1-(0.1/2))
+
+  ci_0.01=c(pval_0.01-q_0.01*sqrt(pval_0.01*(1-pval_0.01)/N),pval_0.01+q_0.01*sqrt(pval_0.01*(1-pval_0.01)/N))
+  ci_0.05=c(pval_0.05-q_0.05*sqrt(pval_0.05*(1-pval_0.05)/N),pval_0.05+q_0.05*sqrt(pval_0.05*(1-pval_0.05)/N))
+  ci_0.1=c(pval_0.1-q_0.1*sqrt(pval_0.1*(1-pval_0.1)/N),pval_0.1+q_0.1*sqrt(pval_0.1*(1-pval_0.1)/N))
+
+  return(c(pval_0.01,ci_0.01,
+           pval_0.05,ci_0.05,
+           pval_0.1,ci_0.1))
 }
 
