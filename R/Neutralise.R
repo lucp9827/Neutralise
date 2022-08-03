d1657be833ff1bebc08e4f6fb7439bcbd7ffb32c
lucp9827 <leyla.kodalci@uhasselt.be>
2022-08-03 12:06:25 +0200

    update for test sims

--- R/Neutralise.R
+++ R/Neutralise.R
@@ -10,7 +10,7 @@
 Neutralise<-function(path,
                      Test=NULL,
                      Data.Generator=NULL,
-                     settings=NULL, B=10000, N=10) {
+                     settings=NULL, B=10000, N=1000) {
 
   if(is.null(Test)&is.null(Data.Generator)&is.null(settings)) {
     # run code that checks directories to see what need to be run: Run_All
