bb999c1f0baf8218d5b24deff664dc706ca60f47
lucp9827 <leyla.kodalci@uhasselt.be>
2023-02-07 13:21:59 +0100

    Create sum_methods.R

---
+++ R/sum_methods.R
@@ -0,0 +1,44 @@
+#'
+#' @export
+## Function to summarize methods
+sum_methods = function(){
+  
+  method.files<-dir(path=paste("methods",sep=""))
+  load(paste("results/neutralisestatus.RData",sep=""))
+  method.exists<-method.files%in%neutralise.status$file.name[
+    (neutralise.status$type=="method")&
+      (neutralise.status$check==TRUE)]
+  
+  text=data.frame()
+  
+  for (i in (1:length(method.files))){
+    
+    filename_temp=method.files[i]
+    
+    filename<-paste("methods/",filename_temp,sep="")
+    
+    con=file(filename,"r")
+    tmp<-readLines(con,n=-1)
+    close(con)
+    
+    name_id = which(tmp=="# NAME")
+    name_txt = tmp[name_id+1]
+    name_txt = gsub('#','',name_txt)
+    
+    des_id = which(tmp=="# DESCRIPTION")
+    des_txt = tmp[des_id+1]
+    des_txt = gsub('#','',des_txt)
+    
+    ref_id = which(tmp=="# REFERENCES")
+    ref_txt = tmp[ref_id+1]
+    ref_txt = gsub('#','',ref_txt)
+    
+    abbriv_txt=gsub('.R','',filename_temp)
+    
+    text_tmp= data.frame(Abbriviation=abbriv_txt,Name=name_txt,Description=des_txt,References=ref_txt)
+    
+    text=rbind(text,text_tmp)
+  }
+  
+  return(text)
+}
